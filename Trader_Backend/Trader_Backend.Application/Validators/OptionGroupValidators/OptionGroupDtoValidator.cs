83b02f724bc5d3ab923197b27291e2cdb0a81435
احمد رائد عبد الفتاح ملاعبه <malabehahmad5@gmail.com>
2026-09-18 17:12:13 +0300

    Add OptionGroupDtoValidator
    Added OptionGroupDtoValidator.cs using FluentValidation to enforce required and max length rules on Value and required, positive rule on CategoryID for OptionGroupDto. Custom Arabic error messages are included for each rule.

---
+++ Trader_Backend/Trader_Backend.Application/Validators/OptionGroupValidators/OptionGroupDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using Trader_Backend.Application.DTOs;
+namespace Trader_Backend.Application.Validators.OptionGroupValidators
+{
+    public class OptionGroupDtoValidator : AbstractValidator<OptionGroupDto>
+    {
+        public OptionGroupDtoValidator()
+        {
+            RuleFor(x => x.Value)
+                .NotEmpty().WithMessage("القيمه مطلوبة.")
+                .MaximumLength(100).WithMessage("يجب ألا يتجاوز طول القيمة 100 حرف.");
+            RuleFor(x => x.CategoryID)
+                .NotEmpty().WithMessage("رقم الفئة مطلوب.")
+                .GreaterThan(0).WithMessage("لا يمكن أن يكون رقم الفئة أقل من 0.");
+        }
+    }
+}
